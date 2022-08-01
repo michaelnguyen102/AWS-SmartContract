514e6138577d3b34bf1633917930db693e6e95f5
Michael Nguyen <mikedallas10@gmail.com>
2022-08-01 10:14:20 -0500

    Added BSC Oracle contract

--- contracts/addresses.js
+++ contracts/addresses.js
@@ -1,5 +1,6 @@
 const addresses = {
-  UniswapPairOracle_HEC_DAI: "0x227CCdfebEA1355E73F1936C8a23Ebb5F0181b3C"
+  UniswapPairOracle_HEC_DAI: "0x18f236bD93AA13e2Bbd8eDc084c8d390AFf4Ee9f",
+  UniswapPairOracle_HEC_DAI_BSC: "0x18f236bD93AA13e2Bbd8eDc084c8d390AFf4Ee9f"
 };
 
 module.exports = addresses;
--- functions/exec.js
+++ functions/exec.js
@@ -9,11 +9,46 @@ async function runTwapUpdate(contract, wallet, label) {
     const overrides = { gasPrice: process.env.DEFAULT_GAS_PRICE, gasLimit: process.env.GAS_LIMIT };
 
     const tx = await contract.update(overrides)
-    const successMessage = `:white_check_mark: Transaction sent https://ftmscan.com/tx/${tx.hash}`;
+    const successMessage = `Calling update: Transaction sent https://ftmscan.com/tx/${tx.hash}`;
     console.log(label, successMessage)
 
     } catch (err) {
       const errorMessage = `:warning: Transaction failed: ${err.message}`;
+      console.error(errorMessage)
+      return err;
+    }
+
+}
+
+async function updateMultiChainOracle(contract, contractBsc, label) {
+    console.log('Sending transaction...', label);
+
+    try {
+    // Specify custom tx overrides, such as gas price https://docs.ethers.io/ethers.js/v5-beta/api-contract.html#overrides
+    const overrides = { gasPrice: process.env.DEFAULT_GAS_PRICE, gasLimit: process.env.GAS_LIMIT };
+
+    //Run update on FTM Oracle
+    const tx = await contract.update(overrides)
+
+    let successMessage = `Calling FTM update func: Transaction sent https://ftmscan.com/tx/${tx.hash}`;
+    console.log(label, successMessage)
+
+    //Retrieves updated data from FTM
+    const blockTimestampLast = await contract.getBlockTimestampLast(overrides);
+    const price0CumulativeLast = await contract.getPrice0CumulativeLast(overrides);
+    const price1CumulativeLast = await contract.getPrice1CumulativeLast(overrides);
+
+    successMessage = `Retrieve data...blockTimestampLast: ${blockTimestampLast}, price0CumulativeLast: ${price0CumulativeLast}, price1CumulativeLast:  ${price1CumulativeLast}`;
+    console.log(label, successMessage)
+
+    //Update BSC Oracle
+     const txBSC = await contractBsc.update(price0CumulativeLast, price1CumulativeLast, blockTimestampLast, overrides)
+     successMessage = `Calling BSC update func: Transaction sent https://bscscan.com/tx/${txBSC.hash}`;
+    console.log(label, successMessage)
+
+
+    } catch (err) {
+      const errorMessage = `Transaction failed: ${err.message}`;
       console.error(errorMessage)
       return err;
     }
@@ -31,28 +66,53 @@ exports.handler = async function() {
 
   const UniswapPairOracle_HEC_DAI_ABI = abis.UniswapPairOracle_HEC_DAI;
   const UniswapPairOracle_HEC_DAI_Address = addresses.UniswapPairOracle_HEC_DAI;
+  const UniswapPairOracle_HEC_DAI_BSC_ABI = abis.UniswapPairOracle_HEC_DAI_BSC;
+  const UniswapPairOracle_HEC_DAI_BSC_Address = addresses.UniswapPairOracle_HEC_DAI_BSC;
   console.log('Contract ABIs loaded');
 
-  // Initialize Ethers wallet
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  // Initialize Fantom wallet
+  const provider = new ethers.providers.JsonRpcProvider(process.env.FTM_RPC_URL);
   let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-  wallet = wallet.connect(provider)
-  console.log('Ethers wallet loaded');
+  // Initialize BSC wallet
+  const providerBSC = new ethers.providers.JsonRpcProvider(process.env.BSC_RPC_URL);
+  let walletBSC = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-  // Load contract
+  wallet = wallet.connect(provider)
+  console.log('Fantom wallet loaded');
+
+  walletBSC = walletBSC.connect(providerBSC)
+  console.log('BSC wallet loaded');
+
+  // Load FTM contract
   const contract_HEC_DAI = new ethers.Contract(
     UniswapPairOracle_HEC_DAI_Address,
     UniswapPairOracle_HEC_DAI_ABI,
     wallet
   )
 
-  console.log('Contract loaded');
+  console.log('Contract UniswapPairOracle_HEC_DAI loaded');
 
-  await runTwapUpdate(    
+  const contract_HEC_DAI_BSC = new ethers.Contract(
+    UniswapPairOracle_HEC_DAI_BSC_Address,
+    UniswapPairOracle_HEC_DAI_BSC_ABI,
+    walletBSC
+  )
+
+  console.log('Contract UniswapPairOracle_HEC_DAI_BSC loaded');
+
+
+  // await runTwapUpdate(    
+  //   contract_HEC_DAI,
+  //   wallet,
+  //   'HEC_DAI: ')
+
+  await updateMultiChainOracle(    
     contract_HEC_DAI,
-    wallet,
-    'HEC_DAI: ')
+    contract_HEC_DAI_BSC, 
+    'updateMultiChainOracle: ')
+
+    
 
   console.log('Completed');
   return true;
